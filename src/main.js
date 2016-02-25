0986df95d18b68d0c9d462b4a7e4bf27d0eb7e73
yancey1204 <yancey1204@gmail.com>
2016-02-25 16:24:49 +0800

    make the search bar of index page animated

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 $(document).foundation();
+$(".close").click(function(){
+  $("form").addClass("fadeOut").one("animationend", function(){
+    $(this).removeClass("fadeOut search-bar-open ");
+  });
+})
+
+$(".show").click(function(){
+  // if ($("form").hasClass("fadeOut")) {
+  //   $("form").removeClass("fadeOut");
+  // }
+  $("form").addClass("fadeIn search-bar-open").one("animationend", function(){
+    $(this).removeClass("fadeIn");
+  });
+})
